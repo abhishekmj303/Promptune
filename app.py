3e39f86b364af02520bdfc4f54db29132d1d52c3
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-03-16 23:27:24 +0530

    frontend patch

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
+	cover = {'title': 'Melody Mix', 'url': ''}
+	playlist = [{'title': 'fffff', 'artist': 'Anyone', 'url': ''}, 
+			 {'title': 'ggggg', 'artist': 'Anyone', 'url': ''},]
+	# return render_template("playlist.html", cover=cover, playlist=playlist)
 	return render_template("index.html")
-
 if __name__ == '__main__':
 	app.run(debug=True)
