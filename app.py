69e7ed08057d94c922e6ac8858ea935559ac31fa
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-03-16 23:50:30 +0530

    html form, login & query submit

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
 	cover = {'title': 'Melody Mix', 'url': ''}
-	playlist = [{'title': 'fffff', 'artist': 'Anyone', 'url': ''}, 
-			 {'title': 'ggggg', 'artist': 'Anyone', 'url': ''},]
-	# return render_template("playlist.html", cover=cover, playlist=playlist)
-	return render_template("index.html")
+	playlist = [{'name': 'fffff', 'artist': 'Anyone', 'url': ''}, 
+			 {'name': 'ggggg', 'artist': 'Anyone', 'url': ''},]
+	return render_template("playlist.html", cover=cover, playlist=playlist)
+	# return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	return render_template('login.html')
+
+@app.route('/query', methods=['POST'])
+def query():
+	return render_template('query.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
